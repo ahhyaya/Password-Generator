b8296f771867379e5bc645cc40f2816109e2fb78
doldol123 <toruig59@gmail.com>
2022-09-29 22:23:47 -0700

    clear all issues including generatPassword function

--- develop/script.js
+++ develop/script.js
@@ -1,7 +1,5 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-    // generateBtn.onclick = generatePassword;
-    // console.log(generateBtn)
 
     var userChoice = [];
     var randomPassword = "";
@@ -13,11 +11,12 @@ var generateBtn = document.querySelector("#generate");
     var uppercaseChar = ["A","B","C","D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     var numericChar = ["0", "1","2","3","4","5","6","7","8","9"];
     var specialChar = [" ", "\"","!","#","$","%","&","\'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
-    
-    
+    var passwordLength = 0;
+    var choiceArr = [];
+
     function passwordCondition() {
-      var choiceArr = [];
-      var passwordLength = prompt("How many characters for your password? (between 8 - 128)");
+      
+      passwordLength = prompt("How many characters for your password? (between 8 - 128)");
       if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
         alert("Invalid Entry!");
         return false;
@@ -38,31 +37,25 @@ var generateBtn = document.querySelector("#generate");
     }
 
 function generatePassword() {
-    // randomPassword = (Math.floor(Math.random() * userChoice-1));
     // get the array of all user choice char 
-    
+    // var finalArr = generatePassword(choiceArr);
     // write a loop to pick a char with a random index from the user choice arr.
     // push each char to the final array
-    for (var i = 0; i < finalArr.length; i++) {
-        var a = choiceArr[(Math.floor(Math.random() * finalArr.length-1))];
-        randomPassword = randomPassword.push(a);
+    for (var i = 0; i < passwordLength; i++) {
+        var a = Math.floor(Math.random() * choiceArr.length);
+        randomPassword = randomPassword + choiceArr[a];
     }
-    // display the password from the array, inside of the div
-    console.log(randomPassword)
-    var randomPassword = document.querySelector("#password");
+    return randomPassword;
 }
 
 // Write password to the #password input
 function writePassword() {
 
   passwordCondition();
-  // var password = generatePassword();
 
-  
-  // passwordText.value = randomPassword;
-
-  // entryValidation();
-
+  var randomPassword = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = randomPassword;
 }
 
 // Add event listener to generate button
